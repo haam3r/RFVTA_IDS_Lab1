32b66ca7875e26527b341d66ba436545fba4cb48
haam3r <haam3r@users.noreply.github.com>
2018-03-01 17:16:53 +0200

    change folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,7 @@ git pull
 apt-get install -y python3-pip jq
 pip3 -U install faker
 
+cd /root/labs/RF_IDS_Lab1_Scripts/scripts
 /root/labs/RF_IDS_Lab1_Scripts/setup.sh
 /root/labs/RF_IDS_Lab1_Scripts/scripts/assign_ips.sh
 /root/labs/simple-lab-wizard/setup_web.sh
