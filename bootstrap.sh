e6bfca2d7d6a0ab1907d4499affc1e0730bf8881
haam3r <haam3r@users.noreply.github.com>
2018-03-01 18:44:37 +0200

    change log file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Bootstrap script for VTA
 export LC_ALL=C
-echo "Start bootstrap" >> /var/log/syslog
+echo "Start bootstrap" >> /root/running/setup.log
 LAB_FOLDER="/root/labs/RF_IDS_Lab1_Scripts"
 
 cd ${LAB_FOLDER}
 git pull
-echo "Did git pull" >> /var/log/syslog
+echo "Did git pull" >> /root/running/setup.log
 apt-get install -y python3-pip jq
 pip3 install faker
 
@@ -14,4 +14,4 @@ pip3 install faker
 /root/labs/RF_IDS_Lab1_Scripts/setup.sh
 #/root/labs/RF_IDS_Lab1_Scripts/scripts/assign_ips.sh
 /root/labs/simple-lab-wizard/setup_web.sh
-echo "Finish bootstrap" >> /var/log/syslog
+echo "Finish bootstrap" >> /root/running/setup.log
