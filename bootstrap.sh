83f5f76d36b5097bd0e6b0040323543eb12042cf
haam3r <haam3r@users.noreply.github.com>
2018-03-01 17:57:30 +0200

    add logging

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Bootstrap script for VTA
-
+echo "Start bootstrap" >> /var/log/syslog
 LAB_FOLDER="/root/labs/RF_IDS_Lab1_Scripts"
 
 cd ${LAB_FOLDER}
 git pull
-
+echo "Did git pull" >> /var/log/syslog
 apt-get install -y python3-pip jq
 pip3 -U install faker
 
@@ -13,3 +13,4 @@ pip3 -U install faker
 /root/labs/RF_IDS_Lab1_Scripts/setup.sh
 #/root/labs/RF_IDS_Lab1_Scripts/scripts/assign_ips.sh
 /root/labs/simple-lab-wizard/setup_web.sh
+echo "Finish bootstrap" >> /var/log/syslog
