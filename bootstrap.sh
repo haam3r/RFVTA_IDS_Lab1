85e9f4c70cb40f3af3612138a65d5e250bc20db7
haam3r <haam3r@users.noreply.github.com>
2018-03-09 17:16:49 +0200

    add ci-modular stuff pulling

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,20 +3,28 @@
 export LC_ALL=C
 echo "Start bootstrap" >> /root/running/setup.log
 LAB_FOLDER="/root/labs/RF_IDS_Lab1_Scripts"
+CI_TARGET="/root/labs/ci-modular-target"
+CI_TARGET_CHECKS="/root/labs/ci-modular-target-checks"
 PRODUCT=$(dmidecode -s system-product-name)
+
+cd ${CI_TARGET}
+git pull
+
+cd ${CI_TARGET_CHECKS}
+git pull
 
 cd ${LAB_FOLDER}
 git pull
-echo "Did git pull" >> /root/running/setup.log
+
+echo "Did git pulls" >> /root/running/setup.log
 apt-get install -y python3-pip jq
 pip3 install faker
 
-#if [ "${PRODUCT}" == "VirtualBox" ]; then
-#       echo "Running in template, doing no further setup"
-#       exit 0
-#fi
+if [ "${PRODUCT}" == "VirtualBox" ]; then
+       echo "Running in template, doing no further setup"
+       exit 0
+fi
 
-#cd /root/labs/RF_IDS_Lab1_Scripts/scripts
 . ${LAB_FOLDER}/setup.sh
 . ${LAB_FOLDER}/scripts/assign_ips.sh
 . ${LAB_FOLDER}/scripts/web.sh
