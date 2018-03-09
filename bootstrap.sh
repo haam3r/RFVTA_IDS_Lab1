13ddf4d5fd0ed27506559d5ee5c825776701a408
haam3r <haam3r@users.noreply.github.com>
2018-03-09 14:21:15 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,10 +11,10 @@ echo "Did git pull" >> /root/running/setup.log
 apt-get install -y python3-pip jq
 pip3 install faker
 
-if [ "${PRODUCT}" == "VirtualBox" ]; then
-       echo "Running in template, doing no further setup"
-       exit 0
-fi
+#if [ "${PRODUCT}" == "VirtualBox" ]; then
+#       echo "Running in template, doing no further setup"
+#       exit 0
+#fi
 
 #cd /root/labs/RF_IDS_Lab1_Scripts/scripts
 . ${LAB_FOLDER}/setup.sh
